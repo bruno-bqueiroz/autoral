f346951d93d91ec9e448c977819e9c5527022fa8
BRUNO BARBOSA <brunobdq32@gmail.com>
2023-02-08 15:19:27 -0400

    docs: tela de cadastro e de login pronta

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import Home from './pages/Home/index'
 import MonthlyStatement from './pages/MonthlyStatement';
 import Enrollment from './pages/Enrollments';
 import SigIn from './pages/SigIn';
+import SigUp from './pages/SigUp';
 function App() {
   return (
    <>
     <Router>
       <Routes>
         <Route path="/" element={<SigIn />} />
+        <Route path="/signUp" element={<SigUp />} />
         <Route path="/home" element={<Home />} />
         <Route path="/resumo" element={<MonthlyStatement />} />
         <Route path="/cadastro" element={<Enrollment />} />
